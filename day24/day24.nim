de5f7682bf048d47c902e771aa1d025e98678c8d
aquova <mail@aquova.net>
2024-12-24 22:07:48 -0600

    Finished day 24

--- day24/day24.nim
+++ day24/day24.nim
@@ -1,4 +1,4 @@
-import options, strscans, strutils, tables
+import algorithm, options, strscans, strutils, tables
 
 type Op = tuple
     gateA, op, gateB: string
@@ -34,27 +34,59 @@ proc solve(gates: var Table[string, Option[bool]], ops: Table[string, Op]): bool
         else:
             result = false
 
-proc combineZs(gates: Table[string, Option[bool]]): int =
+proc combineRegister(gates: Table[string, Option[bool]], reg: string): (int, int) =
     var idx = 0
     var value = ""
     while true:
-        let gate = "z" & align($idx, 2, '0')
+        let gate = reg & align($idx, 2, '0')
         if gates.contains(gate):
             let bit = if gates[gate].get(): "1" else: "0"
             value = bit & value
             inc(idx)
         else:
             break
-    return value.parseBinInt()
+    return (value.parseBinInt(), idx - 1)
 
 proc day24p1*(input: string): string =
     let sections = input.split("\n\n")
     var gateValues = sections[0].parseInitial()
     var ops: Table[string, Op]
     parseGates(sections[1], gateValues, ops)
-    while not solve(gateValues, ops):
-        discard
-    return $gateValues.combineZs()
+    while not solve(gateValues, ops): discard
+    return $gateValues.combineRegister("z")[0]
 
 proc day24p2*(input: string): string =
-    discard
+    let sections = input.split("\n\n")
+    var gateValues = sections[0].parseInitial()
+    var ops: Table[string, Op]
+    parseGates(sections[1], gateValues, ops)
+    while not solve(gateValues, ops): discard
+    let (_, maxZ) = gateValues.combineRegister("z")
+
+    var wrong: seq[string]
+    for output, op in ops.pairs():
+        if op.op != "XOR" and output[0] == 'z' and output != "z" & $maxZ:
+            # Only XOR can output to a Z register
+            if output notin wrong:
+                wrong.add(output)
+        elif op.op == "XOR" and (op.gateA[0] notin ['x', 'y', 'z'] and op.gateB[0] notin ['x', 'y', 'z'] and output[0] notin ['x', 'y', 'z']):
+            # XORs can only be involved with X, Y, or Z registers
+            if output notin wrong:
+                wrong.add(output)
+        elif op.op == "OR":
+            # Only AND can input into OR
+            if op.gateA in ops and ops[op.gateA].op != "AND" and op.gateA notin wrong:
+                let prev = ops[op.gateA]
+                if prev.gateA[0] in ['x', 'y']:
+                    wrong.add(op.gateA)
+                    for key, value in ops.pairs():
+                        if (value.gateA == prev.gateA or value.gateB == prev.gateA) and value.op != prev.op:
+                            wrong.add(key)
+            if op.gateB in ops and ops[op.gateB].op != "AND" and op.gateB notin wrong:
+                let prev = ops[op.gateB]
+                if prev.gateA[0] in ['x', 'y']:
+                    wrong.add(op.gateB)
+                    for key, value in ops.pairs():
+                        if (value.gateA == prev.gateA or value.gateB == prev.gateA) and value.op != prev.op:
+                            wrong.add(key)
+    return wrong.sorted().join(",")
